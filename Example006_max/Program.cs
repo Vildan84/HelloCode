24f5746ba21a2c62eb00aebd76ecb3fe3334db8e
Vildan <vildan84@mail.ru>
2022-07-06 22:19:16 +0300

    Example006 max

---
+++ Example006_max/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 7;
+int b = 2;
+int c = 5;
+int d = 9;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+
+Console.Write("MAX: ");
+Console.WriteLine(max);
+
