213947db78c89fbf0f8629e1b9c1f7a47d66c906
Vildan <vildan84@mail.ru>
2022-07-06 22:09:33 +0300

    Example005 Greeting

---
+++ Example005_Greeting/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter name of user: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Oh my god it's Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+
+}
