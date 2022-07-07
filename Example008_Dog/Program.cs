db3cff64b6bd999c5522be9baa51ef12b358f2c5
Vildan <vildan84@mail.ru>
2022-07-07 13:12:54 +0300

    Example008 Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int distance = 200;
+int FirstFriendSpeed = 5;
+int SecondFriendSpeed = 7;
+int DogSpeed = 10;
+int friend = 2;
+int time = 0;
+
+while(distance > 10)
+{
+    if(friend == 2)
+    {
+        time = distance / (SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+    else
+    {
+        time = distance / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count += 1;
+Console.WriteLine(count);
+
+}
+
+
+
+
