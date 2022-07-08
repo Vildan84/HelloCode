6acb433dab895c94881f8dee129e5126ffca5cea
Vildan <vildan84@mail.ru>
2022-07-08 21:15:50 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Вторая задача
+
+Console.WriteLine("Enter number: ");
+
+int number = Console.Readline();
+
+if(number == 1)
+{
+    Console.WriteLine("Monday"); 
+}
+else if(number == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if(number == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if(number == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if(number == 5)
+{
+    Console.WriteLine("Friday");
+}
+else if(number == 6)
+{
+    Console.WriteLine("Saturday");
+}
+else if(number == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else
+{
+    Console.Writeline("Enter number from 1 to 7");
+}
